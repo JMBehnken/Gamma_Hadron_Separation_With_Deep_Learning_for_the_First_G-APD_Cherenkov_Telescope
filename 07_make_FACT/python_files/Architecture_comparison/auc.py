2c624f6e7c85f3f9949e202127b1736463e08735
Jan Moritz Behnken <jbehnken@phobos.app.tu-dortmund.de>
2017-09-14 12:08:09 +0200

    Update

--- 07_make_FACT/python_files/Architecture_comparison/auc.py
+++
@@ -1,9 +0,0 @@
-import pandas as pd
-import os
-
-p='/fhgfs/users/jbehnken/make_Data/architectures'
-folders=[os.path.join(p, path) for path in os.listdir(p)]
-
-for folder in folders:
-	df=pd.read_csv(folder+'/'+folder.split('_')[-1]+'_Hyperparameter.csv')
-	print(df['Auc'].max())
